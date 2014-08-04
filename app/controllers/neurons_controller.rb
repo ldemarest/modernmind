fede782f80628f683572feecaaa1dd5f5b7e2389
Spencer Tang <spencer.tang12@gmail.com>
2014-08-04 16:46:58 -0400

    save images

--- app/controllers/neurons_controller.rb
+++ app/controllers/neurons_controller.rb
@@ -4,12 +4,15 @@ class NeuronsController < ApplicationController
   end
 
   def create
-    @user = User.find(session[:user_id]) if session[:user_id]
+    binding.pry
+    # @user = User.find(session[:user_id]) if session[:user_id]
 
-    unless @user.neurons.find_by(neuron: params[:id])
-      @neuron = Neuron.create(neuron_params) 
-      @user.neurons << @neruon
-    end
+    # unless @user.neurons.find_by(neuron: params[:id])
+    #   @neuron = Neuron.create(neuron_params) 
+    #   @user.neurons << @neruon
+    # end
+    @neuron = Neuron.new(image: params[:image])
+
    
     respond_to do |format|
       if @neuron.save
@@ -20,10 +23,17 @@ class NeuronsController < ApplicationController
     end
   end
 
+    def zip
+    @neuron = Neuron.all.first.image
+
+  end
+
   private
 
   def neuron_params
     params.require(:neuron).permit(:contributor_id, :name, :id, :image)
   end 
+
+
 
 end
