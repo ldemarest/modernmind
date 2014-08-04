057671de43801e8476cea6ec82ad8c1c687b8915
juliataitz <juliataitz@gmail.com>
2014-08-04 15:07:15 -0400

    create method neuron controller

--- app/controllers/neurons_controller.rb
+++ app/controllers/neurons_controller.rb
@@ -4,6 +4,26 @@ class NeuronsController < ApplicationController
   end
 
   def create
+    @user = User.find(session[:user_id]) if session[:user_id]
 
+    unless @user.neurons.find_by(neuron: params[:id])
+      @neuron = Neuron.create(neuron_params) 
+      @user.neurons << @neruon
+    end
+   
+    respond_to do |format|
+      if @neuron.save
+        format.json { render json: @neuron}
+      else
+        format.json { render json: "Error", status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+
+  def neuron_params
+    params.require(:neuron).permit(:contributor_id, :name, :id, :image)
+  end 
+
 end
