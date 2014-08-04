466bcecc0aabbda9efac53ed36821e8b7397c08b
juliataitz <juliataitz@gmail.com>
2014-08-04 15:01:40 -0400

    adds image to neuron

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804182453) do
+ActiveRecord::Schema.define(version: 20140804190034) do
 
   create_table "minds", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140804182453) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "contributor_id"
+    t.text     "image"
   end
 
   create_table "users", force: true do |t|
