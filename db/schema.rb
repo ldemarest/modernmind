a1c36bf0549dfeb825270f93103b87edf7d60b5b
juliataitz <juliataitz@gmail.com>
2014-08-05 22:03:21 -0400

    removes creator_id from minds

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806014249) do
+ActiveRecord::Schema.define(version: 20140806020229) do
 
   create_table "minds", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "creator_id"
   end
 
   create_table "neurons", force: true do |t|
