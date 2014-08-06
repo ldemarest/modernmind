bbb13b382bb574849a5080d9483b52e500b9d15a
juliataitz <juliataitz@gmail.com>
2014-08-06 13:39:25 -0400

    add mind maker to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806144836) do
+ActiveRecord::Schema.define(version: 20140806173650) do
 
   create_table "minds", force: true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140806144836) do
     t.string   "uid"
     t.string   "email"
     t.string   "image"
+    t.boolean  "mind_maker", default: false
   end
 
 end
