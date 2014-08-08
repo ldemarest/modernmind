14a39d19e4bf1980239a89275740f0ab909ed81a
Spencer Tang <spencer.tang12@gmail.com>
2014-08-07 20:25:45 -0400

    merge

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,13 +13,16 @@ class StaticPagesController < ApplicationController
   end
 
   def publicminds
+    binding.pry
 
       @minds = Mind.all.where(:public => true)
+      @completed_minds = @minds.completed_minds_public
+      @incomplete_minds = @minds.incomplete_minds_public
 
   end
 
   def greatminds
-    
+
   
 
   end
--- app/models/mind.rb
+++ app/models/mind.rb
@@ -3,4 +3,5 @@ class Mind < ActiveRecord::Base
     has_many :users, :through => :user_minds
     has_many :neurons, :through => :user_minds
 
+
 end
