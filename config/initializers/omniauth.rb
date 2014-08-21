bb3962726c1b70d14e879195057ca8c57e2aaa3c
ldemarest <demarest.luke@gmail.com>
2014-08-21 15:21:42 -0400

    configures facebook authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+	if ENV['RAILS_ENV'] == 'production'
+		HOST = 'www.modernmind.herokuapp.com'
+		URL = 'http://#{HOST}/auth/facebook/callback'
+	else
+		HOST= 'localhost:3000'
+		URL= 'http://#{HOST}/auth/facebook/callback'
+	end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+  callback_url: URL
+}
 end
